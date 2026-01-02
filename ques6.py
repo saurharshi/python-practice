cc11e264fedbdb0a0ee7f4054c6a108dc6336d3a
Harshita Prasad <122971643+saurharshi@users.noreply.github.com>
2026-01-02 16:22:50 +0530

    Organized GFG files into folders

--- ques6.py
+++
@@ -1,5 +0,0 @@
-distance=395000
-timetaken=9000
-new_time=timetaken/3600
-speed=(distance)/(new_time)
-print(speed)
--- ques7.py
+++
@@ -1,9 +0,0 @@
-# dimesions of the pool in metres
-l=12
-b=7
-h=2
-volume=l*b*h
-print(f"volume of pool is {volume}")
-time_flow=17
-time_hours=volume/time_flow
-print(f"It will take {time_hours}to empty your pool")
